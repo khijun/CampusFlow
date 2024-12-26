c9da283d23474379a68109b5f43bf4bcb66e9089
HyukJin98 <dlgurwls4321@gmail.com>
2024-12-26 17:23:01 +0900

    찐막 수정

--- src/main/java/edu/du/campusflow/entity/ChangeRequest.java
+++ src/main/java/edu/du/campusflow/entity/ChangeRequest.java
@@ -24,19 +24,19 @@ public class ChangeRequest {
 
     @ManyToOne
     @JoinColumn(name = "before_code",referencedColumnName = "code_id")
-    private CommonCode before_code;        // 변경 전 코드
+    private CommonCode beforeCode;        // 변경 전 코드
 
     @ManyToOne
     @JoinColumn(name = "after_code",referencedColumnName = "code_id")
-    private CommonCode after_code;            // 변경 후 코드
+    private CommonCode afterCode;            // 변경 후 코드
 
     @ManyToOne
     @JoinColumn(name = "academic_status",referencedColumnName = "code_id")
-    private CommonCode academic_status;                 // 학적 상태
+    private CommonCode academicStatus;                 // 학적 상태
 
     @ManyToOne
     @JoinColumn(name = "application_status",referencedColumnName = "code_id")
-    private CommonCode application_status;            // 신청 상태
+    private CommonCode applicationStatus;            // 신청 상태
 
-    private LocalDateTime request_date;                  // 신청 일자
+    private LocalDateTime requestDate;                  // 신청 일자
 }
