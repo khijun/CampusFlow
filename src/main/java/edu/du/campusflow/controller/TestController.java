ec10aa6fe3a3c4e4c374e5da580ebb9bce3c603f
kheejun00 <khijun00@gmail.com>
2024-12-26 17:50:31 +0900

    파일 업로드기능 테스트중

--- src/main/java/edu/du/campusflow/controller/TestController.java
+++ src/main/java/edu/du/campusflow/controller/TestController.java
@@ -1,12 +1,16 @@
 package edu.du.campusflow.controller;
 
 import edu.du.campusflow.service.AuthService;
+import edu.du.campusflow.service.FileUploadService;
 import edu.du.campusflow.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/test")
@@ -14,12 +18,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     private final AuthService authService;
     private final MemberService memberService;
+    private final FileUploadService fileUploadService;
 
     @GetMapping
     public String index(Model model) {
         Long memberId = authService.getCurrentMemberId();
         model.addAttribute("member", memberService.findByMemberId(memberId));
         return "index";
+    }
+
+    @PostMapping("/files")
+    public String fileUpload(@RequestParam("file") MultipartFile file, Model model) {
+        fileUploadService.saveFile(file);
+        return "redirect:/test";
     }
 
 }
--- src/main/java/edu/du/campusflow/service/FileUploadService.java
+++ src/main/java/edu/du/campusflow/service/FileUploadService.java
@@ -2,6 +2,7 @@ package edu.du.campusflow.service;
 
 import edu.du.campusflow.define.FileDefine;
 import edu.du.campusflow.entity.FileInfo;
+import edu.du.campusflow.exception.EmptyFileException;
 import edu.du.campusflow.repository.FileInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,13 +10,16 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class FileUploadService {
 
-    private final FileInfoRepository uploadedFileRepository;
+    private final FileInfoRepository fileInfoRepository;
     private final AuthService authService;
 
     // 파일 확장자에 따른 파일 경로를 얻는 메서드
@@ -50,31 +54,39 @@ public class FileUploadService {
     @Transactional
     public void saveFile(MultipartFile[] files) {
         for (MultipartFile file : files) {
-            if (file.isEmpty()) continue;
-            try {
-                String uuid = String.valueOf(System.nanoTime());
-                String realName = file.getOriginalFilename();
-                String type = getFileExtension(file.getOriginalFilename());
-                String path = getPathByExtension(file.getOriginalFilename());
-                double fileSize = file.getSize();
-                LocalDateTime createAt = LocalDateTime.now();
-                FileInfo fileInfo = FileInfo.builder()
-                        .member(authService.getCurrentMember())
-                        .fileUuid(uuid)
-                        .fileName(realName)
-                        .fileType(type)
-                        .filePath(path)
-                        .fileSize(fileSize)
-                        .createdAt(createAt)
-                        .build();
-            } catch (RuntimeException e) {
-                throw new RuntimeException(e);
-            }
+
         }
     }
     @Transactional
-    public void saveFile(MultipartFile file) {
-        saveFile(new MultipartFile[]{file});
+    public void saveFile(MultipartFile file){
+        if (file.isEmpty()) throw new EmptyFileException("저장할 파일이 비어있습니다");
+        try {
+            String uuid = String.valueOf(System.nanoTime());
+            String realName = file.getOriginalFilename();
+            String type = getFileExtension(file.getOriginalFilename());
+            String path = getPathByExtension(file.getOriginalFilename()); // 파일을 저장할 폴더 위치
+            createDirectoryIfNotExists(path);
+            double fileSize = file.getSize();
+            LocalDateTime createAt = LocalDateTime.now();
+            FileInfo fileInfo = FileInfo.builder()
+                    .member(authService.getCurrentMember())
+                    .fileUuid(uuid)
+                    .fileName(realName)
+                    .fileType(type)
+                    .filePath(path)
+                    .fileSize(fileSize)
+                    .createdAt(createAt)
+                    .build();
+            Path savePath = Paths.get(path, uuid);
+            try{
+                file.transferTo(savePath);
+            } catch (IOException e) {
+                throw new RuntimeException("파일 저장에 실패함.");
+            }
+            fileInfoRepository.save(fileInfo);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void createDirectoryIfNotExists(String path) {
