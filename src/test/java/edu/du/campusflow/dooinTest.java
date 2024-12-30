0662c77f123a5e391c722e2284beec5ae5865717
Dooin <dooin1861@naver.com>
2024-12-30 17:31:42 +0900

    테스트 생성

---
+++ src/test/java/edu/du/campusflow/dooinTest.java
@@ -0,0 +1,13 @@
+package edu.du.campusflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class dooinTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
