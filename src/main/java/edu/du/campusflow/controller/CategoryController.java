810d526b7456a9203144965e7771e9846e1960ee
kheejun00 <kheejun00@gmail.com>
2025-01-06 10:51:11 +0900

    카테고리 컨트롤러 추가

--- src/main/java/edu/du/campusflow/controller/CategoryController.java
+++ src/main/java/edu/du/campusflow/controller/CategoryController.java
@@ -3,11 +3,11 @@ package edu.du.campusflow.controller;
 import edu.du.campusflow.entity.Category;
 import edu.du.campusflow.service.AuthService;
 import edu.du.campusflow.service.CategoryService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,7 +22,7 @@ public class CategoryController {
 
     @GetMapping
     public ResponseEntity<?> getCategory() {
-        List<Category> categories = categoryService.findByType();
-        return null;
+        List<Category> categories = categoryService.findByType(authService.getCurrentMember().getMemberType());
+        return ResponseEntity.status(HttpStatus.OK).body(categories);
     }
 }
