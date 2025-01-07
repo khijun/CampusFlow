9b28f0c95c3ff4f4ee047070d1b92c675649509d
kheejun00 <kheejun00@gmail.com>
2025-01-07 11:23:03 +0900

    로그인시 현재 멤버의 정보 출력 및 디자인 소폭 변경

--- src/main/java/edu/du/campusflow/controller/MainController.java
+++ src/main/java/edu/du/campusflow/controller/MainController.java
@@ -1,5 +1,7 @@
 package edu.du.campusflow.controller;
 
+import edu.du.campusflow.dto.MemberDTO;
+import edu.du.campusflow.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class MainController {
 
+   private final AuthService authService;
+
+   public MainController(AuthService authService) {
+      this.authService = authService;
+   }
+
    @GetMapping("/")
    public String front_main(Model model) {
-      model.addAttribute()
+      model.addAttribute("memberDTO", MemberDTO.fromEntity(authService.getCurrentMember()));
       return "view/main_view/front_main"; // 메인 페이지
    }
 
--- src/main/java/edu/du/campusflow/dto/MemberDTO.java
+++ src/main/java/edu/du/campusflow/dto/MemberDTO.java
@@ -14,13 +14,15 @@ public class MemberDTO {
     private Long memberId;
     private String name;
     private String deptName;
-    private String tel;
     private String memberTypeStr;
 
     public static MemberDTO fromEntity(Member member) {
+        if(member == null) return null;
         return MemberDTO.builder()
                 .memberId(member.getMemberId())
                 .name(member.getName())
-                .deptName(member.getDept().get)
+                .deptName(member.getDept().getDeptName())
+                .memberTypeStr(member.getMemberType().getCodeName())
+                .build();
     }
 }
