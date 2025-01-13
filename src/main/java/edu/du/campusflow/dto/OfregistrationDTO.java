fed32ac607421e8cfc36495e4668a5ef0427f18a
Floater <kdh09263@gmail.com>
2025-01-13 11:59:08 +0900

    1.10 12:32 OfregistrationDTO 고침

--- src/main/java/edu/du/campusflow/dto/OfregistrationDTO.java
+++ src/main/java/edu/du/campusflow/dto/OfregistrationDTO.java
@@ -1,5 +1,6 @@
 package edu.du.campusflow.dto;
 
+import edu.du.campusflow.entity.CommonCode;
 import edu.du.campusflow.entity.Lecture;
 import edu.du.campusflow.entity.Ofregistration;
 import lombok.AllArgsConstructor;
@@ -16,25 +17,25 @@ public class OfregistrationDTO {
     private Long lectureId;              // 강의 ID
     private String lectureName;          // 강의명
     private String deptName;             // 학과명
-    private String subjectName;          // 교과목명
-    private Integer grade;               // 학년
+    private String grade;               // 학년
     private String subjectType;          // 이수구분 (전공필수, 교양 등)
-    private Integer credits;             // 학점
+    private Integer subjectCredits;             // 학점
 
     // 교수 정보
-    private  String  memberId;           // 교수 아이디
-    private String professorName;        // 담당교수
+    private Long memberId;           // 교수 아이디
+    private String name;             // 담당교수(member테이블)
 
     // 강의 세부 정보
-    private String lectureTime;          // 강의시간
+    private String lectureDay;          //강의 요일
+    private String startTime;           //시작 시간
+    private String endTime;             //종료 시간
     private String facilityName;         // 강의실
     private Integer maxStudents;         // 정원
     private Integer currentStudents;     // 현재 수강인원
     private String dayNight;             // 주야구분
-    private Long syllabusFileId;         // 강의계획서 파일 ID
 
     // 수강신청 상태 정보
-    private String registrationStatus;   // 수강신청 상태
-    private Boolean isRetake;            // 재수강 여부
+    private String regStatus;   // 수강신청 상태
+    private Boolean retake;            // 재수강 여부
 
 }
