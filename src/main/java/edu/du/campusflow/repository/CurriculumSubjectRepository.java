0a685f56023605bcfa0edd349dcbbd0d2c857076
iku701 <iku701@naver.com>
2025-01-14 16:27:23 +0900

    0114_충돌수정

--- src/main/java/edu/du/campusflow/repository/CurriculumSubjectRepository.java
+++ src/main/java/edu/du/campusflow/repository/CurriculumSubjectRepository.java
@@ -2,7 +2,6 @@ package edu.du.campusflow.repository;
 
 import edu.du.campusflow.entity.CurriculumSubject;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CurriculumSubjectRepository extends JpaRepository<CurriculumSubject, Long>, JpaSpecificationExecutor<CurriculumSubject> {
+public interface CurriculumSubjectRepository extends JpaRepository<CurriculumSubject, Long> {
 }
