7b985954f0c66ed8a6c3e21225ef6db7bd834433
Floater <kdh09263@gmail.com>
2025-01-15 15:56:36 +0900

    1.15 3:55 병합완료 TODO:신청버튼 누르면 상태변경시키기

--- src/main/java/edu/du/campusflow/repository/LectureTimeRepository.java
+++ src/main/java/edu/du/campusflow/repository/LectureTimeRepository.java
@@ -2,6 +2,7 @@ package edu.du.campusflow.repository;
 
 import edu.du.campusflow.entity.CommonCode;
 import edu.du.campusflow.entity.Facility;
+import edu.du.campusflow.entity.Lecture;
 import edu.du.campusflow.entity.LectureTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 
 public interface LectureTimeRepository extends JpaRepository<LectureTime, Long> {
     List<LectureTime> findByStartTimeAndEndTimeAndLectureWeek_Week(CommonCode startTime, CommonCode endTime, Integer week);
+    List<LectureTime> findByLectureWeek_Lecture(Lecture lecture);
 }
