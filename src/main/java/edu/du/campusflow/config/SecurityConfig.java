be908abcc4495096c30445a462f948a5e21e4288
kheejun00 <kheejun00@gmail.com>
2025-01-18 18:53:47 +0900

    시큐리티 필터 로그인 관련 페이지 권한 수정

--- src/main/java/edu/du/campusflow/config/SecurityConfig.java
+++ src/main/java/edu/du/campusflow/config/SecurityConfig.java
@@ -59,10 +59,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/js/**", "/css/**").permitAll() // 정적 리소스 허용
+                .antMatchers("/js/**", "/css/**", "/assets/**").permitAll() // 정적 리소스 허용
 //                .antMatchers("/login").permitAll() // 로그인 페이지 허용
-//                .anyRequest().authenticated() // 그 외 요청은 인증 필요
-                .anyRequest().permitAll() // 실험코드
+                .anyRequest().authenticated() // 그 외 요청은 인증 필요
+//                .anyRequest().permitAll() // 실험코드
                 .and()
                 .headers()
                 .frameOptions().sameOrigin() // X-Frame-Options 설정
@@ -75,7 +75,6 @@ public class SecurityConfig {
                 .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
-                .permitAll()
                 .and()
                 .csrf().disable();  // 임시 코드
 
