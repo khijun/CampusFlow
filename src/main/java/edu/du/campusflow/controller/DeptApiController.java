a12f9013d2dbd0a4304aea9816fc47518ac70077
kheejun00 <kheejun00@gmail.com>
2025-01-20 09:51:05 +0900

    학과 생성 기능 추가

--- src/main/java/edu/du/campusflow/controller/DeptApiController.java
+++ src/main/java/edu/du/campusflow/controller/DeptApiController.java
@@ -1,14 +1,13 @@
 package edu.du.campusflow.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.du.campusflow.dto.DeptCreateRequest;
 import edu.du.campusflow.dto.DeptDTO;
 import edu.du.campusflow.dto.DeptSearchFilter;
 import edu.du.campusflow.service.DeptService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +28,14 @@ public class DeptApiController {
             filter = DeptSearchFilter.builder().build();
         }
         return DeptDTO.fromEntityList(deptService.findAllWithFilter(filter));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> postDept(@RequestBody DeptCreateRequest deptCreateRequest) {
+        try{
+            return ResponseEntity.ok(deptService.create(deptCreateRequest));
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
