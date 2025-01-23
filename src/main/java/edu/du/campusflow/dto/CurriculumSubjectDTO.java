1da81360c9be8e03d0d16a8c6213b10fd2731c6f
jungpil99 <kaka87355377@gmail.com>
2025-01-23 13:41:16 +0900

    0123 pull + 강의 개설 페이지 검색부분 수정

--- src/main/java/edu/du/campusflow/dto/CurriculumSubjectDTO.java
+++ src/main/java/edu/du/campusflow/dto/CurriculumSubjectDTO.java
@@ -11,4 +11,7 @@ public class CurriculumSubjectDTO {
     private String subjectCredits;
     private String semesterName;
     private String subjectTypeName; // 코드 이름 추가
+
+    private String deptName;             //학과 명
+    private Integer curriculumYear;      //연도
 }
