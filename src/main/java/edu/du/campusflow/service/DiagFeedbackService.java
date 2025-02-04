7aeeaeb25ea504e7c9880d1bb44c39dcf583b784
kheejun00 <kheejun00@gmail.com>
2025-02-04 14:55:24 +0900

    학생 공통 css 적용 및 간격 수정

--- src/main/java/edu/du/campusflow/service/DiagFeedbackService.java
+++ src/main/java/edu/du/campusflow/service/DiagFeedbackService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-// feature/evaluation ㅁㄴㅇㅁㄴㅇㅁㅁㄴㅇ
+// feature/evaluation.css ㅁㄴㅇㅁㄴㅇㅁㅁㄴㅇ
 
 @Service
 @RequiredArgsConstructor
