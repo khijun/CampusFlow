ca878b9eae16dcdb26e9473af3a2730defa69b1b
Dooin <dooin1861@naver.com>
2025-02-05 02:43:48 +0900

    메인 뷰, 이미지 추가 / 컨트롤러에서 공지, 게시판 받아서 뷰로 던지는 코드 추가 / 메인컨텐트 뷰 스타일을 메인컨텐트.css로 이동

--- src/main/java/edu/du/campusflow/controller/MainController.java
+++ src/main/java/edu/du/campusflow/controller/MainController.java
@@ -1,9 +1,15 @@
 package edu.du.campusflow.controller;
 
 import edu.du.campusflow.dto.MemberDTO;
+import edu.du.campusflow.entity.Notice;
+import edu.du.campusflow.entity.Post;
 import edu.du.campusflow.service.AuthService;
+import edu.du.campusflow.service.NoticeService;
+import edu.du.campusflow.service.PostService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +20,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
    private final AuthService authService;
+   private final NoticeService noticeService;
+   private final PostService postService;
 
    @GetMapping("/")
    public String frontMain(Model model) {
@@ -23,7 +31,17 @@ public class MainController {
 
    @GetMapping("/iframe/main/main-content")
    public String mainContent(Model model) {
+      // 현재 사용자 정보
       model.addAttribute("memberDTO", MemberDTO.fromEntity(authService.getCurrentMember()));
+
+      // 최신 공지사항 5개 가져오기
+      Page<Notice> latestNotices = noticeService.getAllNotices(PageRequest.of(0, 5));
+      model.addAttribute("noticePage", latestNotices);
+
+      // 로그인중인 사용자의 학과 게시글만 5개 가져오기
+      Page<Post> posts = postService.getPostsByDepartment(0, 5);
+      model.addAttribute("postPage", posts);
+
       return "view/iframe/main/main_content";
    }
 
@@ -31,4 +49,6 @@ public class MainController {
    public String accessDenied(Model model) {
       return "view/exception/access-denied";
    }
+
+
 }
