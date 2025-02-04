844a0d57cf173db9141cf1b3b8a116a03e6261d8
Hyukjin98 <dlgurwls4321@gmail.com>
2025-02-04 16:28:52 +0900

    정보 css 약간 수정

--- src/main/java/edu/du/campusflow/repository/EducationInfoRepository.java
+++ src/main/java/edu/du/campusflow/repository/EducationInfoRepository.java
@@ -2,10 +2,12 @@ package edu.du.campusflow.repository;
 
 import edu.du.campusflow.entity.EducationInfo;
 import edu.du.campusflow.entity.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface EducationInfoRepository extends JpaRepository<EducationInfo,Long> {
+    @EntityGraph(attributePaths = {"graduationStatus"})
     List<EducationInfo> findByMember_MemberId(Long memberId);
 }
